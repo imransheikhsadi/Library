71a30cf071f88d8866ef789fa57176b03c22c531
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-03-27 03:17:34 +0600

    project done but have some performance issue

--- Element ON Screen/on-screen/on_screen.js
+++ Element ON Screen/on-screen/on_screen.js
@@ -1,7 +1,47 @@
 class OnScreen {
-  constructor() {
-    console.log('project is initilized');
+  constructor(items) {
+    this.store = []
+    this.init(items)
   }
+  init = (items)=>{
+    window.addEventListener('scroll',()=>{
+      items.forEach((item,i)=>{
+        document.querySelectorAll(item.element).forEach((element,i)=>{
+          const visible = this.checkPosition(element);
+            this.handleDuplicate(item,element,visible)
+        })
+      })
+    })
+
+  }
+
+  handleDuplicate(item,element,visible){
+    if (this.store[element.id] === undefined && visible) {
+        this.store[element.id] = visible;
+        item.callback.in(element)
+    }else if (this.store[element.id] && !visible){
+      this.store[element.id] = visible;
+      item.callback.out(element)
+    }else if(!this.store[element.id] && visible){
+      this.store[element.id] = visible;
+      if (item.repeat === undefined || item.repeat) {
+        console.log(item);
+        item.callback.in(element)
+      }
+    }
+  }
+
+  checkPosition(element){
+     const rect = element.getBoundingClientRect();
+     const html = document.documentElement;
+
+     return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    )
+   }
 }
 
 export default OnScreen;
--- Element ON Screen/src/script/index.js
+++ Element ON Screen/src/script/index.js
@@ -1,4 +1,14 @@
 import '../style/sass/main.scss';
 import OnScreen from '../../on-screen/on_screen';
 
-new OnScreen;
+new OnScreen([
+    {
+        element: '[data-visible]',
+        callback: {
+            in: (element)=>{
+                // element.style.transform = 'translateX(30px)'
+            },
+            out: ()=>{console.log('item out')}
+        }
+    }
+]);
